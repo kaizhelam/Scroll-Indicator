a9cc2e15edfdf185fc271edd54bf40245ee0b1af
kaizhelam <lamkaizhe2001@gmail.com>
2021-11-13 15:18:14 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onscroll = function(){
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById('myBar').style.width = scrolled + "%";
+}
